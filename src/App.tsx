5c759f6bdd6ee56b2bb0a921c020356270a3e3b9
Tazeem041 <124208833+Tazeem041@users.noreply.github.com>
2025-01-10 22:21:27 +0530

    "sample CV"

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <div className="w-1/2 border border-4 border-slate-900">
+        <div className="m-12 border-t-4 border-slate-800 ">
+          <div className="mt-4">
+            <h1 className="text-4xl font-mono mt-4">Thajeem</h1>
+            <p className="text-lg text-green-600 font-sans mt-2">
+              FrontEnd developer
+            </p>
+            <p>City-Hyderabad</p>
+            <p>Ph-134567893</p>
+            <p>shaik@me.123</p>
+          </div>
+          <div>
+            <h5 className="text-lg text-green-600 font-sans mt-4">SKILLS</h5>
+            <p>React.js,JavaScript,HTML,CSS</p>
+          </div>
+          <div>
+            <h6 className="text-lg text-green-600 font-sans mt-4">Eduction</h6>
+            <h5 className="text-blue-800 font-bold">
+              School Name,Location, Degree
+            </h5>
+          </div>
+          <div>
+            <h6 className="text-lg text-green-600 font-sans mt-4">
+              Experience
+            </h6>
+            <h5 className="text-blue-800 font-bold ">
+              Company Name,Location,Job Title
+            </h5>
+            <p className="mb-4">Month 20xx to Month 20xx</p>
+            <ul>
+              <li>List of achivment</li>
+              <li>List of achivment</li>
+              <li>List of achivment</li>
+            </ul>
+            <p className="mt-2">Skills-React.js,JavaScript,HTML,CSS</p>
+            <h5 className="text-blue-800 font-bold mt-2">
+              Company Name ,Location-,Job Title
+            </h5>
+            <p className="mb-4">Month 20xx to Month 20xx</p>
+            <ul>
+              <li>List of achivment</li>
+              <li>List of achivment</li>
+              <li>List of achivment</li>
+            </ul>
+            <p className="mt-2">Skills-React.js,JavaScript,HTML,CSS</p>
+          </div>
+          <div>
+            <h4 className="text-lg text-green-600 font-sans mt-4">
+              Accors The Internet
+            </h4>
+            <p>Add your LinkedIn, GitHub Profile Links</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
